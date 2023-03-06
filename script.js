5bf364890fb02462e7625f279f77290d6a970191
Boivinos <boivinantonin@gmail.com>
2023-03-06 20:16:07 +0100

    Finished winner function, next step is end function + draw function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let croix = document.getElementsByClassName("croix");
 let rond = document.getElementsByClassName("rond");
 let caseArea = document.getElementsByClassName("caseArea");
-
+let grille = document.getElementById("grille");
+let gameOver = false;
 let gameStatus = ["", "", "", "", "", "", "", "", ""]
 
 for (let i = 0; i < croix.length; i++) {
@@ -11,35 +12,49 @@ for (let i = 0; i < croix.length; i++) {
 
 function winner() {
     for (let i = 0; i < gameStatus.length; i++) {
+
         if (gameStatus[i] != "") {
             if (gameStatus[i] == gameStatus[i + 1] && gameStatus[i] == gameStatus[i + 2]) {
                 console.log(`${gameStatus[i]} is the winner !`)
+                gameOver = true;
             }
             if (gameStatus[i] == gameStatus[i + 3] && gameStatus[i] == gameStatus[i + 6]) {
                 console.log(`${gameStatus[i]} is the winner !`)
+                gameOver = true;
+            }
+            if (gameStatus[i] == gameStatus[i + 4] && gameStatus[i] == gameStatus[i + 8]) {
+                console.log(`${gameStatus[i]} is the winner !`)
+                gameOver = true;
+            }
+            if (i == 4 && gameStatus[i] == gameStatus[i + 2] && gameStatus[i] == gameStatus[i - 2]) {
+                console.log(`${gameStatus[i]} is the winner !`)
+                gameOver = true;
             }
         }
-    }
 
+    }
 }
+
 
 
 function turn(shape) {
 
     for (let i = 0; i < caseArea.length; i++) {
         caseArea[i].onclick = function () {
-            if (shape === "croix") {
+            if (gameStatus[i] == "" && shape === "croix") {
                 croix[i].style.visibility = "visible";
                 gameStatus.splice(i, 1, "Red")
-                console.log(gameStatus)
                 turn("rond")
                 winner()
-            } else {
+                console.log(gameOver)
+            } else if (gameStatus[i] == "") {
                 rond[i].style.visibility = "visible";
                 gameStatus.splice(i, 1, "Green")
-                console.log(gameStatus)
                 turn("croix")
                 winner()
+                console.log(gameOver)
+            } else {
+                console.log("Cette case est déjà occupée")
             }
         }
 
@@ -48,7 +63,10 @@ function turn(shape) {
 
 
 
-turn("croix")
+
+grille.onclick = turn("croix")
+
+
 
 
 
