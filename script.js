d76f3e7d62ff266a137a5bd3499f7448361ea21d
Boivinos <boivinantonin@gmail.com>
2023-03-07 15:52:50 +0100

    Fixed everything

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-let croix = document.getElementsByClassName("croix");
-let rond = document.getElementsByClassName("rond");
 let caseArea = document.getElementsByClassName("caseArea");
+let tourJoueur = document.getElementById("tourJoueur")
 let grille = document.getElementById("grille");
-let startGame = document.getElementById("start-game")
+let startGame = document.querySelectorAll('[id=start-game]')
 let gameOver = false;
-let players = ["Red", "Green"]
 let gameStatus = ["", "", "", "", "", "", "", "", ""]
+let symboles = { red: "X", green: "O" }
+let alternance = { red: "green", green: "red" }
+let traduction = { red: "rouge", green: "vert" }
 
-for (let i = 0; i < croix.length; i++) {
-    croix[i].style.visibility = "hidden";
-    rond[i].style.visibility = "hidden";
+function winnerDisplay(color) {
+    tourJoueur.innerHTML = `<h2 class="titre-${color}">Victoire du joueur ${traduction[color]} !</h2>`
 }
 
+function turnDisplay(color) {
+    tourJoueur.innerHTML = `<h2 class="titre-${color}">C'est au tour du joueur ${traduction[color]} !</h2>`
+}
 
+function endGame() {
+    for (let i = 0; i < caseArea.length; i++) {
+        caseArea[i].onclick = function () {
+        }
+    }
+}
 
 function winner() {
 
@@ -20,60 +29,83 @@ function winner() {
 
         if (gameStatus[i] != "") {
             if ((i == 0 || i == 3 || i == 6) && gameStatus[i] == gameStatus[i + 1] && gameStatus[i] == gameStatus[i + 2]) {
-                console.log(`${gameStatus[i]} is the winner !`)
+                winnerDisplay(gameStatus[i])
                 gameOver = true;
+                endGame()
             }
             if (gameStatus[i] == gameStatus[i + 3] && gameStatus[i] == gameStatus[i + 6]) {
-                console.log(`${gameStatus[i]} is the winner !`)
+                winnerDisplay(gameStatus[i])
                 gameOver = true;
+                endGame()
             }
             if (gameStatus[i] == gameStatus[i + 4] && gameStatus[i] == gameStatus[i + 8]) {
-                console.log(`${gameStatus[i]} is the winner !`)
+                winnerDisplay(gameStatus[i])
                 gameOver = true;
+                endGame()
             }
             if (i == 4 && gameStatus[i] == gameStatus[i + 2] && gameStatus[i] == gameStatus[i - 2]) {
-                console.log(`${gameStatus[i]} is the winner !`)
+                winnerDisplay(gameStatus[i])
                 gameOver = true;
+                endGame()
             }
         }
     }
-    console.log("finbouclewinner " + gameOver)
 }
 
-function turn(shape) {
-    console.log("début boucle tour " + gameOver)
-    if (gameOver == true) {
+function coup(color, rank) {
+    caseArea[rank].innerHTML = `<div class="${color}" id="${color}">${symboles[color]}</div>`
+    gameStatus.splice(rank, 1, color)
+    winner()
+    turn(alternance[color])
+}
 
-        console.log("Fin du jeu");
-        return;
-    } else if (gameOver == false) {
-        for (let i = 0; i < caseArea.length; i++) {
+function turn(color) {
+
+    for (let i = 0; i < caseArea.length; i++) {
+
+        if (gameOver == false && gameStatus[i] == "") {
             caseArea[i].onclick = function () {
-                if (gameStatus[i] == "" && shape === "croix" && gameOver == false) {
-                    croix[i].style.visibility = "visible";
-                    gameStatus.splice(i, 1, "Red")
-                    turn("rond")
-                    winner()
-                } else if (gameStatus[i] == "" && gameOver == false) {
-                    rond[i].style.visibility = "visible";
-                    gameStatus.splice(i, 1, "Green")
-                    turn("croix")
-                    winner()
-                } else if (gameStatus[i] == "X" || gameStatus[i] == "O") {
-                    console.log("Cette case est déjà occupée")
-                }
+                turnDisplay(alternance[color])
+                coup(color, i);
             }
         }
-
     }
 }
 
-startGame.onclick = function () {
-    turn("croix")
-}
+document.querySelectorAll('button').forEach(occurence => {
+    occurence.addEventListener('click', (e) => {
+        let color = occurence.innerHTML.toLowerCase()
+        console.log(color)
+        turn(color);
+        turnDisplay(color);
+    });
+});
 
-let test = document.getElementById("test")
-test.innerHTML = "X"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
